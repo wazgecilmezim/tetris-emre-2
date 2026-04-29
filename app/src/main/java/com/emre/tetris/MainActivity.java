a38ce2653cf175936c16cd14eeb0218c4a9a5ee4
wazgecilmezim <filmvidyoizle@gmail.com>
2026-04-29 21:50:30 +0300

    Add MainActivity to load local HTML in WebView

---
+++ app/src/main/java/com/emre/tetris/MainActivity.java
@@ -0,0 +1,17 @@
+package com.emre.tetris;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}

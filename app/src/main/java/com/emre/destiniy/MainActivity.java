0787dc9121683910958c21c9bc8946a826305ab9
wazgecilmezim <filmvidyoizle@gmail.com>
2026-05-01 20:49:55 +0300

    Add MainActivity to load local HTML file in WebView

---
+++ app/src/main/java/com/emre/destiniy/MainActivity.java
@@ -0,0 +1,20 @@
+package com.emre.destiniy;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
